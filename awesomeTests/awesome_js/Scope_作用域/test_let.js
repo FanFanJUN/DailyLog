9c1c1f9b7eded3a27726dc57b35a2ff4463b97da
LiCai <1981824361@qq.com>
2021-03-04 14:49:29 +0800

    模拟var的伪块作用域

--- awesomeTests/awesome_js/Scope_作用域/test_let.js
+++ awesomeTests/awesome_js/Scope_作用域/test_let.js
@@ -1,4 +1,12 @@
 for (let index = 0; index < 3; index++) {
-    console.log(index);
+    console.log(index); // 0 1 2
 }
-console.log(index); // ReferenceError: index is not defined
+// console.log(index); // ReferenceError: index is not defined
+
+// setTimeout
+
+for (let index = 0; index < 3; index++) {
+    setTimeout(() => {
+        console.log(index); // 0 1 2
+    }, 2000);
+}
--- awesomeTests/awesome_js/Scope_作用域/test_var.js
+++ awesomeTests/awesome_js/Scope_作用域/test_var.js
@@ -4,3 +4,20 @@ for (var index = 0; index < 3; index++) {
 }
 console.log('final');
 console.log(index); // 3
+
+/* setTimeout结果 */
+
+for (var index = 0; index < 3; index++) {
+    setTimeout(() => {
+        console.log(index); //3 3 3
+    }, 2000); 
+    
+}
+// setTimeout + 函数自调用  模拟var的伪块作用域
+for (var index = 0; index < 3; index++) {
+    (function (a) {
+        setTimeout(() => {
+            console.log(a); //1 2 3 
+        }, 2000); 
+    })(index)
+}
